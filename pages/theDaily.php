60c11b6b8a1c8552274bed42ae6b08e4193f8c4e
root <tlankford88@gmail.com>
2016-01-18 16:03:39 -0500

    The carousel for the daily quiz will now automatically load to the day based upon which day the quiz is visited

--- pages/theDaily.php
+++ pages/theDaily.php
@@ -37,33 +37,49 @@ getDaily();
 <!--display the daily -->
 <?php
 /*
-for ($i=0;$i<sizeof($_SESSION['questions']);$i++){
-	echo "<br>".$_SESSION['categories'][$_SESSION['ctr']]." tip of the day: ";
-	echo "<br>description: ". $_SESSION['questions'][$_SESSION['ctr']];
-	echo '<br><button type="button" class="btn btn-success" data-toggle="displayAnswer" title="% correct" data-content="'.$_SESSION['answers'][$_SESSION['ctr']].'">answer</button>';
-	echo "<br><br><br>";
-	$_SESSION['ctr']++;
-}
-*/
+   for ($i=0;$i<sizeof($_SESSION['questions']);$i++){
+   echo "<br>".$_SESSION['categories'][$_SESSION['ctr']]." tip of the day: ";
+   echo "<br>description: ". $_SESSION['questions'][$_SESSION['ctr']];
+   echo '<br><button type="button" class="btn btn-success" data-toggle="displayAnswer" title="% correct" data-content="'.$_SESSION['answers'][$_SESSION['ctr']].'">answer</button>';
+   echo "<br><br><br>";
+   $_SESSION['ctr']++;
+   }
+ */
 ?>
 <br>
 <div class="carouselWrapper">  
-<div id="myCarousel" class="carousel slide"data-ride="carousel" data-interval="false">
+<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
 <div class="carousel-inner">
-
-<div class="active item">
-<p>Sunday</p>
 <?php
-for ($i=0;$i<sizeof($_SESSION['questions']);$i++){
-	echo "<br><p>".$_SESSION['categories'][$_SESSION['ctr']]." tip of the day: </p>";
-	echo "<br><p>description: ". $_SESSION['questions'][$_SESSION['ctr']]."</p>";
-	echo '<br><p><button type="button" class="btn btn-success" data-toggle="displayAnswer" title="% correct" data-content="'.$_SESSION['answers'][$_SESSION['ctr']].'">answer</button></p>';
-	echo "<br><br><br>";
-	$_SESSION['ctr']++;
-}
-?>
-</div>
+$days = array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday");
+foreach ($days as $day){
+	if (Date("l") == $day){
+		echo "<div class='active item'>";
+	}
+	else{
+		echo "<div class='item'>";
+	}
+	echo 
+		"
+		<p>".$day."</p>
+		<p>Questions here</p>";
+	for ($i=0;$i<sizeof($_SESSION['questions']);$i++){
+		echo "<br><p>".$_SESSION['categories'][$_SESSION['ctr']]." tip of the day: </p>";
+		echo "<br><p>description: ". $_SESSION['questions'][$_SESSION['ctr']]."</p>";
+		echo '<br><p><button type="button" class="btn btn-success" data-toggle="displayAnswer" title="% correct" data-content="'.$_SESSION['answers'][$_SESSION['ctr']].'">answer</button></p>';
+		echo "<br><br><br>";
+		$_SESSION['ctr']++;
 
+	}
+		$_SESSION['ctr']=0;
+		echo "<br>";
+		echo "</div>";
+
+
+}
+
+?>
+<!--
 <div class="item">
 <p>Monday</p>
 <p>Questions here</p>
@@ -93,11 +109,11 @@ for ($i=0;$i<sizeof($_SESSION['questions']);$i++){
 <p>Saturday</p>
 <p>Questions here</p>
 </div>
-
+-->
 </div>
 </div>
 </div>
- <!-- left and right controls -->
+<!-- left and right controls -->
 <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
 <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 <span class="sr-only">previous</span>
@@ -106,92 +122,6 @@ for ($i=0;$i<sizeof($_SESSION['questions']);$i++){
 <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 <span class="sr-only">next</span>
 </a>
-<!-- 
-<div class="container">
-<div id="mycarousel" class="carousel slide" data-ride="carousel" data-interval="false">
-<ol class="carousel-indicators">
-<li data-target="#mycarousel" data-slide-to="0" class="active"></li>
-<li data-target="#mycarousel" data-slide-to="1"></li>
-<li data-target="#mycarousel" data-slide-to="2"></li>
-<li data-target="#mycarousel" data-slide-to="3"></li>
-</ol>
-
-<div class="carousel-inner" role="listbox">
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<div class="item active">
-<img src="img_chania.jpg" alt="chania"/>
-<div class="carousel-caption">
-<h3>chania</h3>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-</div>
-
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-</div>
-
-<div class="item">
-<div class="carousel-caption">
-<h3>chania</h3>
-<p>the atmosphere in chania has a touch of florence and venice.</p>
-</div>
-</div>
-
-<div class="item">
-<div class="carousel-caption">
-<h3>flowers</h3>
-<p>beatiful flowers in kolymbari, crete.</p>
-</div>
-</div>
-
-<div class="item">
-<div class="carousel-caption">
-<h3>flowers</h3>
-<p>beatiful flowers in kolymbari, crete.</p>
-</div>
-</div>
-</div>
-
-</div>
-</div>
-
-
--->
 
 
 
