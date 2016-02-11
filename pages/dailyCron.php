1f3d321573232167e9492fb8916ca7849658a815
root <tlankford88@gmail.com>
2016-02-10 23:13:28 -0500

    Switching away from user instigated daily activation to using cron jobs. This is the rough draft

---
+++ pages/dailyCron.php
@@ -0,0 +1,21 @@
+<?php
+require "regulate.php";
+connectToSQL();
+$query = "SET time_zone = '-5:00'";
+$stmt = mysqli_query( $GLOBALS['conn'], $query);
+$query =file_get_contents("../sql/createNewDaily.php"); 
+$stmt = mysqli_query( $GLOBALS['conn'], $query);
+//TODO: make this a transaction so 2 users updating a daily cannot happen at the same time
+//mysqli_stmt_execute($stmt);
+
+//Verify failure or success
+if(!$stmt){
+	echo("Error description: " . mysqli_error($GLOBALS['conn']));
+}
+else{
+// echo "Query Success";
+}
+$query= file_get_contents("../sql/deleteOldDaily.sql");
+$stmt = mysqli_query( $GLOBALS['conn'], $query);
+closeSQLConnection();
+?>
